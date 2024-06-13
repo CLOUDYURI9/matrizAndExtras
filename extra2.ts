7f85f418c0708fdcfd2cfeee323c491e7bf95c57
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 16:44:11 -0300

    Update extra2.ts

--- extra2.ts
+++ extra2.ts
@@ -5,9 +5,15 @@ se algum dos valores é igual a média dos mesmos.
 Nome: Yuri Dinato da Silva
 */
 
+/*
+Fazer um programa que receba um conjunto de 10 valores 
+inteiros, armazene em um array, verifique
+se algum dos valores é igual a média dos mesmos.
+Nome: Yuri Dinato da Silva
+*/
 const teclado = require(`prompt-sync`)();
 
-let array: number[] = new Array(10);
+let array: number[] = new Array(10)
 let media: number = 0;
 
 for (let x = 1; x <= 10; x++) {
@@ -16,9 +22,16 @@ for (let x = 1; x <= 10; x++) {
 }
 
 media /= 10;
-
+let j = 0;
 for (let i = 1; i <= 10; i++) {
     if (array[i] == media) {
-        console.log(`O valor ${array[i]} localizado no espaço ${i}, é o mesmo da média das notas, a média dos valores é ${media}.`);
+        console.log(`O valor ${array[i]} localizado no espaço ${i}, é o mesmo da média das notas, a média dos valores é ${media}.`)
+    }
+    if(media != array[i]){
+    j++
+    }
+    if(j == 1){
+        console.log(`Nenhuma das notas é igual a média`);
     }
 }
+
