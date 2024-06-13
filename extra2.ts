715d1baba1e53e3a6e59e58a5b98dd7bef7e0c7e
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 15:51:48 -0300

    Update extra2.ts

--- extra2.ts
+++ extra2.ts
@@ -4,22 +4,21 @@ inteiros, armazene em um array, verifique
 se algum dos valores é igual a média dos mesmos.
 Nome: Yuri Dinato da Silva
 */
+
 const teclado = require(`prompt-sync`)();
 
 let array: number[] = new Array(10)
+let media: number = 0
 
 for (let x = 1; x <= 10; x++) {
-    array[x] = parseFloat(teclado(`Digite a nota ${x}: `));
+    array[x] = parseInt(teclado(`Digite a nota ${x}: `));
+    media += array[x]
 }
 
-let media: number = 0
-for (let o = 1; o <= 10; o++) {
-    media += array[o]
-}
-media /= 10
+media /= 10;
 
 for (let i = 1; i <= 10; i++) {
     if (array[i] == media) {
-        console.log(`O valor ${array[i]} localizado no espaço ${i}, é o mesmo da média dos valores, a média dos valores é ${media}.`)
+        console.log(`O valor ${array[i]} localizado no espaço ${i}, é o mesmo da média das notas, a média dos valores é ${media}.`)
     }
 }
