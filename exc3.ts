5c03169f340582bf689bf053e5bc2daa0c0775e1
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 16:32:12 -0300

    Update exc3.ts

--- exc3.ts
+++ exc3.ts
@@ -8,16 +8,16 @@ console.clear();
 const teclado = require(`Prompt-sync`)();
 
 let minhaMatriz: number[][] = [];
-let caixa = 2
-let espaco = 2
+let caixa = 2;
+let espaco = 2;
 
 for ( let z = 0; z < caixa; z++) {
     minhaMatriz[z]= [];
 
     for ( let x = 0; x < espaco; x++){
-        let n: number = teclado(`Digite o número que vai estar no enderço [${z}, ${x}] da matriz: `)
+        let n: number = teclado(`Digite o número que vai estar no enderço [${z}, ${x}] da matriz: `);
 
         minhaMatriz[z][x] = n;
     }
 }
-console.log(minhaMatriz)
+console.log(minhaMatriz);
