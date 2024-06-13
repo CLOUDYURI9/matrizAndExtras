fb9489d6abe7ceae49396271bd838ab654b7b703
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 14:15:57 -0300

    Update extra1.js

--- extra1.js
+++ extra1.js
@@ -5,12 +5,10 @@ Nome: Yuri Dinato da Silva
 */
 var teclado = require("prompt-sync")();
 var array = new Array(5);
+var media = 0;
 for (var x = 1; x <= 5; x++) {
     array[x] = parseFloat(teclado("Digite a nota ".concat(x, ": ")));
-}
-var media = 0;
-for (var o = 1; o <= 5; o++) {
-    media += array[o];
+    media += array[x];
 }
 media /= 5;
 console.log("A m\u00E9dia das cinco notas \u00E9: ".concat(media));
