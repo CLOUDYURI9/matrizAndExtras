a23c32030a1fba055aa639922aac140d64fff3d8
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 11:14:24 -0300

    Add files via upload

---
+++ exc8.js
@@ -0,0 +1,23 @@
+/*
+8- Crie uma matriz 4x4 com números aleatórios entre 0 e 20.
+Em seguida, exiba a matriz na tela e encontre o maior
+elemento da matriz e a posição em que ele se encontra.
+Nome: Yuri Dinato da Silva
+*/
+var teclado = require("prompt-sync")();
+var matrizDois = new Array(4);
+var maior = 0;
+var coluna = 0;
+var linha = 0;
+for (var i = 0; i < 4; i++) {
+    matrizDois[i] = new Array(4);
+    for (var j = 0; j < 4; j++) {
+        matrizDois[i][j] = (Math.floor(Math.random() * 20));
+        if (matrizDois[i][j] > maior) {
+            maior = matrizDois[i][j];
+            coluna = j;
+            linha = i;
+        }
+    }
+}
+console.log("o maior n\u00FAmero \u00E9 ".concat(maior, " e est\u00E1 encontrado na posi\u00E7\u00E3o: [").concat(linha, ", ").concat(coluna, "]"));
---
+++ exc8.ts
@@ -0,0 +1,28 @@
+/*
+8- Crie uma matriz 4x4 com números aleatórios entre 0 e 20.
+Em seguida, exiba a matriz na tela e encontre o maior
+elemento da matriz e a posição em que ele se encontra.
+Nome: Yuri Dinato da Silva
+*/
+
+const teclado = require(`prompt-sync`)();
+let matrizDois: number[][] = new Array(4);
+let maior = 0;
+let coluna = 0;
+let linha = 0;
+
+
+for (let i = 0; i < 4; i++) {
+
+    matrizDois[i] = new Array(4);
+    for (let j = 0; j < 4; j++) {
+        matrizDois[i][j] = (Math.floor(Math.random() * 20))
+        if (matrizDois[i][j] > maior) {
+            maior = matrizDois[i][j];
+            coluna = j;
+            linha = i;
+        }
+    }
+}
+
+console.log(`o maior número é ${maior} e está encontrado na posição: [${linha}, ${coluna}]`);
