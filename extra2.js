6a346602f145380da2e66dd0a92b385da0ebd170
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 15:52:24 -0300

    Update extra2.js

--- extra2.js
+++ extra2.js
@@ -6,16 +6,14 @@ Nome: Yuri Dinato da Silva
 */
 var teclado = require("prompt-sync")();
 var array = new Array(10);
-for (var x = 1; x <= 10; x++) {
-    array[x] = parseFloat(teclado("Digite a nota ".concat(x, ": ")));
-}
 var media = 0;
-for (var o = 1; o <= 10; o++) {
-    media += array[o];
+for (var x = 1; x <= 10; x++) {
+    array[x] = parseInt(teclado("Digite a nota ".concat(x, ": ")));
+    media += array[x];
 }
 media /= 10;
 for (var i = 1; i <= 10; i++) {
     if (array[i] == media) {
-        console.log("O valor ".concat(array[i], " localizado no espa\u00E7o ").concat(i, ", \u00E9 o mesmo da m\u00E9dia dos valores, a m\u00E9dia dos valores \u00E9 ").concat(media, "."));
+        console.log("O valor ".concat(array[i], " localizado no espa\u00E7o ").concat(i, ", \u00E9 o mesmo da m\u00E9dia das notas, a m\u00E9dia dos valores \u00E9 ").concat(media, "."));
     }
 }
