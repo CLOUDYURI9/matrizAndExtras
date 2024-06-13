3f7653dd6972e8668ff0c668db0cc9b01d342371
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 13:31:45 -0300

    Update exc9.js

--- exc9.js
+++ exc9.js
@@ -12,7 +12,7 @@ for (var i = 0; i < 6; i++) {
     for (var j = 0; j < 6; j++) {
         var numero = parseInt(teclado("Digite o n\u00FAmero que vai estar no endere\u00E7o[".concat(i, ", ").concat(j, "]da matriz: ")));
         matrizDois[i][j] = numero;
-        if (j == 0 || j == 2 || j == 4) {
+        if (j % 2 == 0) {
             resultado += matrizDois[i][j];
         }
     }
