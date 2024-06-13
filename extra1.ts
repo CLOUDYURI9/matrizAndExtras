497b7480dd2ac83d2eb7a9358497949ee2fa0c84
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2024-06-13 16:27:31 -0300

    Update extra1.ts

--- extra1.ts
+++ extra1.ts
@@ -4,12 +4,12 @@ devolve a média do aluno.
 Nome: Yuri Dinato da Silva
 */
 
-let array: number [] = new Array(5)
-let media: number = 0
+let array: number [] = new Array(5);
+let media: number = 0;
 for(let x = 1; x <= 5; x++){
     array[x] = parseFloat(teclado(`Digite a nota ${x}: `));
-    media += array[x]
+    media += array[x];
 }
 
-media /= 5
+media /= 5;
 console.log(`A média das cinco notas é: ${media}`);
